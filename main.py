ede698394b74fdece94c2367c2b70e5aa8b70f47
Luís Manuel Fernandes Pereira <lumafepe@gmail.com>
2021-10-13 18:06:04 +0000

    ficheiros

---
+++ main.py
@@ -0,0 +1,63 @@
+import pygame
+import random
+from triangle import Pmedio, appendT,appendC, distancia, verficaConecçoes
+
+#cenas iniciais
+pygame.font.init()
+pygame.init()
+screen = pygame.display.set_mode((1000, 1000),0,1)
+pygame.display.flip()
+pygame.display.set_caption('simulations')
+running = True
+myfont = pygame.font.SysFont('arial', 15)
+
+#C=[(500,650),(600,600),(500,350),(400,400),(400,600),(600,400),(650,500),(350,500)]#nodos
+C=[]#nodos
+L=[]#linhas
+nNodos=int(input("numero de nodos?"))
+
+def setpos(n,C):
+    while len(C)<n:
+        appendC(C,(random.randint(100,900),random.randint(100,900)))
+
+def putCircles(Cords,n,cor,display):
+    pygame.draw.circle(display,cor,Cords,15)
+    textsurface = myfont.render(str(n+1), False, (255, 255, 255))
+    display.blit(textsurface,(Cords[0]-7,Cords[1]-7))
+    pygame.display.update()
+
+def putLines(circulo1,circulo2,cor,display):
+    pygame.draw.line(display,cor,circulo1,circulo2,2)
+    pygame.display.update()
+
+def ligaTodos(C,display,L,nNodos):
+    while len(L)<nNodos*1.5:#10 linhas
+        j=i=random.randint(0,len(C)-1)
+        while j==i:
+            j=random.randint(0,len(C)-1)
+        appendT(L,(i,distancia(C[i],C[j]),j))
+    for elem in L:
+        putLines(C[elem[0]],C[elem[2]],(0,255,255),display)
+        textsurface = myfont.render(str(elem[1]), False, (255, 255, 255))
+        display.blit(textsurface,Pmedio(C[elem[0]],C[elem[2]]))
+    for i in range(len(C)):
+        putCircles(C[i],i,(255,0,0),screen)
+        
+
+#gera nodos e conecçoes
+setpos(nNodos,C)
+ligaTodos(C,screen,L,nNodos)
+
+
+#nodos vazios
+'''
+while not verficaConecçoes(C,L):
+    setpos(nNodos,C)
+    ligaTodos(C,screen,L,nNodos)
+'''
+while running:
+    for event in pygame.event.get():
+        pygame.display.update()
+        if event.type == pygame.QUIT:
+            running = False
+     
---
+++ triangle.py
@@ -0,0 +1,58 @@
+import pygame
+import math
+
+def somaVetores(vetor1,vetor2):
+    return (vetor1[0]+vetor2[0],vetor1[1]+vetor2[1])
+"""tentativa de setas 
+def defpontos(circulo1,circulo2):
+    vetorDiretor=(circulo2[0]-circulo1[0],-(circulo2[1]-circulo1[1]))
+    anglo = math.atan2(vetorDiretor[0],vetorDiretor[1])
+    distance1 = math.sqrt((vetorDiretor[0]**2)+vetorDiretor[1]**2)-15
+    ponto1 = somaVetores(circulo1,(distance1*math.cos(anglo),-(distance1*math.sin(anglo))))
+    pontoI = somaVetores(circulo1,((distance1-7)*math.cos(anglo),-((distance1-7)*math.sin(anglo))))
+    vectorPerpendicular = (vetorDiretor[1],-vetorDiretor[0])
+    anglo2 = math.atan2(vectorPerpendicular[0],vectorPerpendicular[1])
+    ponto2 = somaVetores(pontoI,(4*math.cos(anglo2),-(4*math.sin(anglo2))))
+    vectorPerpendicular = (-vetorDiretor[1],vetorDiretor[0])
+    anglo3 = math.atan2(vectorPerpendicular[0],vectorPerpendicular[1])
+    ponto3 = somaVetores(pontoI,(4*math.cos(anglo3),-(4*math.sin(anglo3))))
+    return [ponto1,ponto2,ponto3]
+
+"""
+
+
+def appendT(L,elem):
+    c=True
+    for i in L:
+        if (i[0]==elem[0] and i[1]==elem[1]) or (i[1]==elem[0] and i[0]==elem[1]):
+            c=False
+            break
+    if c:
+        L.append(elem)
+    
+def Pmedio(p1,p2):
+    return (round((p1[0]+p2[0])/2),round((p1[1]+p2[1])/2))
+
+def appendC(C,elem):
+    c=True
+    for i in C:
+        if math.sqrt((i[0]-elem[0])**2+(i[1]-elem[1])**2)<100:
+            c=False
+            break
+    if c:
+        C.append(elem)
+
+def verficaConecçoes(C,L):
+    for i in range(len(C)):
+        apareceu=False
+        for j in L:
+            if j[0]==i or j[2]==i:
+                apareceu=True
+                print('f')
+                break
+        if not apareceu:
+            return False
+    return True
+
+def distancia(elem,i):
+    return round(math.sqrt((i[0]-elem[0])**2+(i[1]-elem[1])**2)/50)
